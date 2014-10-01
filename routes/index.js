1826ef58d89ae2dd3af646bc03c5a9cdbb468b28
Alex Reardon <alexreardon@gmail.com>
2014-10-01 12:36:44 +1000

    about page creation

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 var _ = require('underscore'),
-	date = require('../util/date'),
-	person = require('../models/person'),
-	config = require('../config');
+    date = require('../util/date'),
+    person = require('../models/person'),
+    config = require('../config');
 
 var scripts = ['/public/js/pages/index.js'];
 
-exports.routes = function(app){
-    app.get('/', function (req, res, next) {
+exports.routes = function(app) {
+    app.get('/', function(req, res, next) {
 
         // coming soon
-        return res.render('index', {});
-	});
+        return res.render('index', {
+            config: config.application,
+            data: date.get_page_data(),
+        });
+    });
 };
